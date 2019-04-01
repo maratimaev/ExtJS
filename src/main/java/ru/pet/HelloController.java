c6d5b2c28827b63f10608c565ae878c9b40c84cb
Marat Imaev <imaevmarat@outlook.com>
2019-04-01 14:31:22 +0500

    redesign front

--- src/main/java/ru/pet/HelloController.java
+++ src/main/java/ru/pet/HelloController.java
@@ -3,7 +3,7 @@ package ru.pet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,6 +19,13 @@ public class HelloController {
 //        [{"id":1, "name":"a", "price":1},{"id":2, "name":"b", "price":2}]
         String list = "[{\"id\":1, \"name\":\"a\", \"price\":1},{\"id\":2, \"name\":\"b\", \"price\":2}]";
         String json = "{\"id\": 1, \"name\": \"aaa\", \"price\": 1}";
+        return list;
+    }
+
+    @GetMapping("/car/{name}")
+    @ResponseBody
+    public String getCarByName(@PathVariable String name, Model model) {
+        String list = "[{\"id\":1, \"name\":\"a\", \"price\":1}]";
         return list;
     }
 }
--- src/main/resources/static/my.js
+++ src/main/resources/static/my.js
@@ -1,54 +1,85 @@
 Ext.application({
     name: 'HelloExt',
     launch: function () {
-        Ext.create('Ext.panel.Panel', {
-            width: 500,
-            height: 360,
-            padding: 10,
-            alias: 'widget.carCatalogView',
-            layout: 'border',
-            items: [
-                {
-                    xtype: 'grid',
-                    region: 'center',
-                    title: 'Пользователи',
-                    height: 200,
-                    width: 400,
-                    store: store,
-                    columns: [{
-                        header: 'Имя',
-                        dataIndex: 'name'
-                    }, {
-                        header: 'price',
-                        dataIndex: 'price'
-                    }]
-                },
-                {
-                    xtype: 'panel',
-                    // html: '<div style="font: normal 18px cursive"><center><font size = "10">Каталог автомобилей</font></center></div>',
-                    region: 'north',
-                    // height: 80,
-                    items:[
-                        {
-                            xtype: 'textfield',
-                            fieldLabel: 'Ваше имя:',
-                            name: 'name'
-                        },
-                        {
-                            xtype: 'button',
-                            text: 'click',
-                            listeners: {
-                                click: function () {
-                                    var text = Ext.ComponentQuery.query('textfield[name=name]')[0].getValue();
-                                    alert(text);
-                                },
-                                scope: this
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype: 'panel',
+                width: 500,
+                height: 360,
+                padding: 10,
+                alias: 'widget.carCatalogView',
+                layout: 'border',
+                items: [{
+                        xtype: 'grid',
+                        region: 'center',
+                        title: 'Пользователи',
+                        id: 'myGrid',
+                        store: store,
+                        columns: [{
+                            header: 'Имя',
+                            dataIndex: 'name'
+                        }, {
+                            header: 'price',
+                            dataIndex: 'price'
+                        }],
+                        listeners: {
+                            scope: this,
+                            itemcontextmenu: function (tree, record, item, index, e, eOpts) {
+                                var menu_grid = new Ext.menu.Menu({
+                                    items:
+                                        [
+                                            {
+                                                text: 'find', handler: function () {
+                                                     var name = record.get('name');
+                                                     alert(name);
+                                                }
+                                            }
+                                        ]
+                                });
+                                var position = [e.getX() - 10, e.getY() - 10];
+                                e.stopEvent();
+                                menu_grid.showAt(position);
                             }
                         }
+                    },
+                    {
+                        xtype: 'panel',
+                        region: 'north',
+                        height: 80,
+                        items: [
+                            {
+                                xtype: 'textfield',
+                                fieldLabel: 'Ваше имя:',
+                                name: 'name'
+                            },
+                            {
+                                xtype: 'button',
+                                text: 'click',
+                                listeners: {
+                                    click: function () {
+                                        var text = Ext.ComponentQuery.query('textfield[name=name]')[0].getValue();
+                                        store.proxy.setUrl('/car/' + text);
+                                        store.load();
+                                    },
+                                    scope: this
+                                }
+                            }
                         ]
-                }
-            ],
-            renderTo: Ext.getBody()
+                    },
+                    {
+                        xtype: 'component',
+                        title: 'Tab',
+                        flex: 1.5,
+                        region: 'south',
+                        autoEl: {
+                            src: 'https://www.google.com/search?igu=1&ei=&q=YOUR+WORD',
+                            tag: 'iframe'
+                        }
+                    }
+                ],
+                renderTo: Ext.getBody()
+            }
         });
     }
 });
@@ -77,4 +108,3 @@ var store = Ext.create('Ext.data.Store', {
         }
     }
 });
-
